a222f6529a292ab0abb95901434c0dac3940fb7b
Melya Tremblay <meltremb@student.42quebec.com>
2023-03-07 16:41:15 -0500

    fixed array

--- ft_free_array.c
+++ ft_free_array.c
@@ -6,7 +6,7 @@
 /*   By: meltremb <meltremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 15:24:17 by meltremb          #+#    #+#             */
-/*   Updated: 2023/03/07 16:19:56 by meltremb         ###   ########.fr       */
+/*   Updated: 2023/03/07 16:40:41 by meltremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ void	ft_free_array(void	***array)
 	if (*array)
 	{
 		i = -1;
-		while (*array[++i])
-			free(*array[i]);
-		//free(*array);
-		//*array = NULL;
+		while ((*array)[++i])
+			free((*array)[i]);
+		free(*array);
+		*array = NULL;
 	}
 }
