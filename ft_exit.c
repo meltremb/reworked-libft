aed9f280d605d00aa9b7a2e0b283df2172536c5b
Melya Tremblay <meltremb@student.42quebec.com>
2023-03-16 11:06:04 -0400

    doesn't add newline if message empty

--- ft_exit.c
+++ ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: meltremb <meltremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 14:33:36 by meltremb          #+#    #+#             */
-/*   Updated: 2023/03/16 10:57:21 by meltremb         ###   ########.fr       */
+/*   Updated: 2023/03/16 11:05:11 by meltremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,12 @@ void	ft_exit(char *message)
 	int	i;
 
 	i = -1;
-	while (message[++i])
-		write(2, &message[i], 1);
-	write(2, "\n", 1);
+	if (ft_strlen(message) > 0)
+	{
+		while (message[++i])
+			write(2, &message[i], 1);
+		write(2, "\n", 1);
+	}
 	close(2);
 	exit(1);
 }
