d85b501a0614a257e2deaea7c4d301add33e670d
Melya Tremblay <meltremb@student.42quebec.com>
2023-04-05 15:55:06 -0400

    str is digit now handles negatives

--- ft_str_isdigit.c
+++ ft_str_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: meltremb <meltremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 10:59:59 by meltremb          #+#    #+#             */
-/*   Updated: 2023/03/29 14:31:08 by meltremb         ###   ########.fr       */
+/*   Updated: 2023/04/05 15:54:48 by meltremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@ int	ft_str_isdigit(char *str)
 {
 	while (*str)
 	{
-		if ((*str >= '0' && *str <= '9') || *str == '-')
+		if (*str == '-')
+			str++;
+		if (*str >= '0' && *str <= '9')
 			str++;
 		else
 			return (0);
