a8090212d1dacaf63836c3057aa93cd15b3a4e36
Melya Tremblay <meltremb@student.42quebec.com>
2023-04-21 15:30:09 -0400

    added a strcmp function to compare strings without given length

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: meltremb <meltremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 13:04:50 by meltremb          #+#    #+#             */
-/*   Updated: 2023/03/29 11:03:21 by meltremb         ###   ########.fr       */
+/*   Updated: 2023/04/21 14:36:59 by meltremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,7 @@ int		ft_toupper(int c);
 int		ft_tolower(int c);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
+int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
