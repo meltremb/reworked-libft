a519541da3affde9d07f7ae49079234461b63785
Melya Tremblay <meltremb@student.42quebec.com>
2023-05-08 13:18:41 -0400

    fixed str_is_digit

--- ft_str_isdigit.c
+++ ft_str_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: meltremb <meltremb@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 10:59:59 by meltremb          #+#    #+#             */
-/*   Updated: 2023/05/01 14:25:17 by meltremb         ###   ########.fr       */
+/*   Updated: 2023/05/08 13:18:25 by meltremb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	ft_str_isdigit(char *str)
 		if (i > 0 && !(str[i] >= '0' && str[i] <= '9'))
 			return (0);
 		else if ((str[i] >= '0' && str[i] <= '9')
-			|| (str[0] == '-' && (str[1] >= '0' && str[i] <= '9')))
+			|| (str[0] == '-' && (str[1] >= '0' && str[1] <= '9')))
 			i++;
 		else
 			return (0);
